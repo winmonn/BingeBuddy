e40cea88570fce73541fb5aa9ee5b85a38fc2c5b
winmonn <winmon19@gmail.com>
2025-05-10 22:28:20 +0800

    Added Notifications Page to Main

--- BingeBuddy/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/BingeBuddy.AssemblyInfo.cs
+++ BingeBuddy/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/BingeBuddy.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BingeBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1a8133b67b37688e04dc326c65c4d3aadeebf130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e95a2ff1e00ab51dc80a3dd8d01a1feabc1e960d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BingeBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BingeBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
